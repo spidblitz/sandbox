5f0ab57d15acdb42decae50356ca3d09768b5bd2
Boldchingis <bboldchingis@gmail.com>
2025-12-07 18:53:15 +0000

    fix: fix race condition and update related files

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -1,3 +1,4 @@
 // 2025-12-06 11:11:58 +08 - feat: add error handling
 // 2025-12-07 04:48:22 +08 - chore: improve error messages
 // 2025-12-07 10:19:11 +08 - fix: fix security vulnerability
+// 2025-12-08 02:53:15 +08 - fix: fix race condition
--- src/utils/logger.js
+++ src/utils/logger.js
@@ -1,2 +1,3 @@
 2025-12-06 11:32:58 +08 - related update
 2025-12-07 04:24:22 +08 - related update
+2025-12-08 02:53:15 +08 - related update
